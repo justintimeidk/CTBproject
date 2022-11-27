625a2c0a81aada8be6da79c9ad5968e8789c8b2a
justintimeidk <119257923+justintimeidk@users.noreply.github.com>
2022-11-27 22:35:06 +0200

    Update JScode.js

--- JScode.js
+++ JScode.js
@@ -1,5 +1,5 @@
 function move(){
     var elem = document.getElementById("btn");
-    elem.style.left=(Math.random()*(45-(-45))+(-45))+"%";
-    elem.style.top=(Math.random()*(45-(-45))+(-45))+"%";
+    elem.style.left=(Math.random()*(90-0)+0)+"%";
+    elem.style.top=(Math.random()*(90-0)+0)+"%";
 }
